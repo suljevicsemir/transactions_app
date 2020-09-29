d026503b0b337d9003b4a5aff5ccae94150f72bb
suljevicsemir <suljevic.semir@gmail.com>
2020-09-29 03:31:37 +0200

    Customer support UI finished. Backend remains.

--- lib/screens/customer_support/screen.dart
+++ lib/screens/customer_support/screen.dart
@@ -13,12 +13,15 @@ class CustomerSupportScreen extends StatefulWidget {
 
 class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
 
+  var _listHeight = 0.8;
+  final _scrollController = ScrollController();
 
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Palette.themeGreen,
         title: Text('Customer support', style: TextStyle(fontWeight: FontWeight.w600, letterSpacing: 1.0, fontFamily: 'Roboto'),),
@@ -36,12 +39,80 @@ class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
           )
         ],
       ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            bottom: MediaQuery.of(context).viewInsets.bottom + 100,
+            child: Container(
+              height: _listHeight * MediaQuery.of(context).size.height,
+              child: Text('Ja sam semir'),
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 0.2 * MediaQuery.of(context).size.height - 50,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  top: BorderSide(
+                                      color: Palette.themeGreen,
+                                      width: 3
+                                  )
+                              )
+                          ),
+                          child: Container(
+                            margin: EdgeInsets.only(left: 20, right: 20),
+                            child: TextField(
+                              onTap: () {
+                                setState(() {
+                                  _listHeight = 0.2;
+                                });
+                              },
+                              decoration: InputDecoration(
+                                hintText: '  Type your query here...',
+                                hintStyle: TextStyle(color: Colors.grey),
+                                focusedBorder: InputBorder.none,
+                                disabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Palette.themeGreen, width: 3)
+                                ),
+                                enabledBorder: InputBorder.none
+                              ),
+                              cursorColor: Palette.themeGreen,
+                              cursorHeight: 26,
+                              cursorWidth: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
   @override
   void initState() {
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
 
