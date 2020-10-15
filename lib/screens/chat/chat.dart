b3603925714dd7b14332ad0ffdf15142b0046764
suljevicsemir <suljevic.semir@gmail.com>
2020-10-16 01:17:29 +0200

    Better manage of chat screen. Removed all unnecessary heights. Screen should be fully dynamic.

--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -23,12 +23,20 @@ class _ChatScreenState extends State<ChatScreen> {
   final _chatService = ChatService();
   final _textFieldController = TextEditingController();
   Stream _chatStream;
+  bool _hasText = false;
 
 
   @override
   void initState() {
     super.initState();
     _chatStream = FirebaseFirestore.instance.collection('chats').doc(widget.chatInfo.chatId).collection('messages').limit(300).snapshots();
+    _textFieldController.addListener(() {
+      setState(() {
+        if(_textFieldController.text.isNotEmpty)
+          _hasText = true;
+        else _hasText = false;
+      });
+    });
   }
 
   @override
@@ -37,17 +45,20 @@ class _ChatScreenState extends State<ChatScreen> {
     super.dispose();
   }
 
-  _sendMessage() async{
-
-    await _chatService.sendMessage(MessageSend(chatId: widget.chatInfo.chatId, messageText: _textFieldController.text, receiverId: widget.chatInfo.userId));
+  Future<void> _sendMessage() async{
+    String text = _textFieldController.text;
+    if(text.isEmpty) return;
     _textFieldController.clear();
+    await _chatService.sendMessage(MessageSend(chatId: widget.chatInfo.chatId, messageText: text, receiverId: widget.chatInfo.userId));
   }
 
   @override
   Widget build(BuildContext context) {
+    print('view insets bottom: ${MediaQuery.of(context).viewInsets.bottom.toString()}');
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
+        elevation: 0.0,
         backgroundColor: Palette.themeGreen,
         title: Text(widget.chatInfo.userDisplayName, style: TextStyle(fontWeight: FontWeight.w600, letterSpacing: 1.0),),
         actions: [
@@ -67,13 +78,14 @@ class _ChatScreenState extends State<ChatScreen> {
           Positioned.fill(
             bottom: MediaQuery.of(context).viewInsets.bottom + 100,
             child: Container(
-              margin: EdgeInsets.only(top: 20),
+             //color: Colors.pink,
+              margin: EdgeInsets.only(top: 10),
               child: StreamBuilder(
                 stream: _chatStream,
                 builder: (context, snapshot) {
                   if(!snapshot.hasData) return CircularProgressIndicator();
                   return Container(
-
+                    //color: Colors.blue,
                     child: ListView.builder(
                       shrinkWrap: true,
                       itemCount: snapshot.data.documents.length,
@@ -89,22 +101,34 @@ class _ChatScreenState extends State<ChatScreen> {
             left: 0,
             right: 0,
             child: Container(
-              height: 0.2 * MediaQuery.of(context).size.height -50,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Flexible(
-                        child: Container(
-
+              decoration: BoxDecoration(
+                  //color: Colors.blue,
+                  border: Border(
+                      top: BorderSide(
+                          width: 0.5,
+                          color: Colors.grey
+                      )
+                  )
+              ),
+              child: Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Flexible(
                           child: Container(
-                            margin: EdgeInsets.only(left: 20, right: 20),
+                            margin: EdgeInsets.only(left: 8),
                             child: TextField(
                               controller: _textFieldController,
                               decoration: InputDecoration(
-                                  hintText: '  Type your query here...',
-                                  hintStyle: TextStyle(color: Colors.grey),
+                                  hintText: 'Type a message...',
+                                  hintStyle: TextStyle(color: Colors.grey[500], fontSize: 19),
                                   focusedBorder: InputBorder.none,
                                   disabledBorder: OutlineInputBorder(
                                       borderSide: BorderSide(color: Palette.themeGreen, width: 3)
@@ -117,21 +141,33 @@ class _ChatScreenState extends State<ChatScreen> {
                             ),
                           ),
                         ),
-                      ),
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 800),
-                        child: IconButton(
-                          splashColor: Colors.red,
-                          splashRadius: 25,
-                          padding: EdgeInsets.all(8),
-                          color: Palette.themeGreen,
-                          onPressed: () async => await _sendMessage(),
-                          icon: Icon(Icons.send),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 800),
+                          child: IconButton(
+                            splashColor: Colors.red,
+                            splashRadius: 25,
+                            padding: EdgeInsets.all(8),
+                            color: Palette.themeGreen,
+                            onPressed: () async => await _sendMessage(),
+                            icon: _hasText == true ? Icon(Icons.send) : Icon(Icons.thumb_up)
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.all(0),
+                          onPressed: () {},
+                          icon: Icon(Icons.animation),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                        IconButton(icon: Icon(Icons.ac_unit), onPressed: () {})
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           )
--- lib/screens/expenses.dart
+++ lib/screens/expenses.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:transactions_app/palette.dart';
 
@@ -9,43 +6,57 @@ class Expenses extends StatefulWidget {
   _ExpensesState createState() => _ExpensesState();
 }
 
-class _ExpensesState extends State<Expenses> with SingleTickerProviderStateMixin  {
+class _ExpensesState extends State<Expenses> with SingleTickerProviderStateMixin, WidgetsBindingObserver  {
 
   AnimationController _controller;
   Animation<Offset> _offsetFloat;
-
+  String text = 'hello';
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 1200)
     );
 
     _offsetFloat = Tween<Offset>(begin: Offset(1.0, 3.0), end: Offset(1.0, 0.0) ).animate(_controller);
-    _offsetFloat.addListener(() {setState(() {
+    _offsetFloat.addListener(() {
+      setState(() {
 
-    });});
-    _controller.forward();
+      });
+    });
   }
 
 
   @override
   void dispose() {
     _controller.dispose();
+    //WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+      super.didChangeAppLifecycleState(state);
+  }
+
+  @override
   void deactivate() {
     super.deactivate();
-    print('deactivate!');
   }
 
   @override
   Widget build(BuildContext context) {
+   if(_offsetFloat.isCompleted) {
+     _controller.reset();
+   }
+   else {
+     _controller.forward();
+   }
+   _controller.forward();
     return SafeArea(
       child: Scaffold(
         backgroundColor: Palette.themeGreen,
@@ -55,7 +66,7 @@ class _ExpensesState extends State<Expenses> with SingleTickerProviderStateMixin
             color: Colors.red,
             width: 100,
             height: 100,
-            child: Center(child: Text('Hellooooo')),
+            child: Center(child: Text(text)),
           ),
         ),
       ),
